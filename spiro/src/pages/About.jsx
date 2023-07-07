755f543afd14e898cd453911b2d3f26bb3a0e4bb
Brune <abrune.osteo@gmail.com>
2023-07-07 22:50:34 +0200

    home and about page Link

---
+++ spiro/src/pages/About.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
+
+function About() {
+	return (
+	  <h1>About page goes here</h1>
+	)
+  }
+
+  export default About
---
+++ spiro/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
+
+function Home() {
+	return (
+	  <h1>Home page goes here</h1>
+	)
+  }
+
+  export default Home
